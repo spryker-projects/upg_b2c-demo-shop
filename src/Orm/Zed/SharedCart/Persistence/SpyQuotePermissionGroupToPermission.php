1e3abea92dcca16c1b4282d008566b5cbf912ec2
upgrader <vladislav.strelchenko@spryker.com>
2023-09-22 07:34:00 +0000

    Applied release group `FRW-606 Fixed deprecations for faker in tests`, RG link http://release.local/release-group/4535

---
+++ src/Orm/Zed/SharedCart/Persistence/SpyQuotePermissionGroupToPermission.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\SharedCart\Persistence;
+
+use Spryker\Zed\SharedCart\Persistence\Propel\AbstractSpyQuotePermissionGroupToPermission as BaseSpyQuotePermissionGroupToPermission;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_quote_permission_group_to_permission' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyQuotePermissionGroupToPermission extends BaseSpyQuotePermissionGroupToPermission
+{
+
+}
