a15a6b4a6915cb945da418cb7cc33cc444cd1672
upgrader <upgrader@spryker.com>
2023-11-17 15:32:57 +0000

    Applied release group `CC-16982 Fixed frontend security issues`, RG link https://api.release.spryker.com/release-group/4215

---
+++ src/Pyz/Zed/TaxProductConnector/TaxProductConnectorDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\TaxProductConnector;
+
+use Spryker\Zed\TaxProductConnector\TaxProductConnectorDependencyProvider as SprykerTaxProductConnectorDependencyProvider;
+use Spryker\Zed\Customer\Communication\Plugin\TaxProductConnector\CustomerAddressShippingAddressValidatorPlugin;
+
+class TaxProductConnectorDependencyProvider extends SprykerTaxProductConnectorDependencyProvider
+{
+    /**
+     * @return array
+     */
+    public function getShippingAddressValidatorPlugins() : array
+    {
+        return [
+            new CustomerAddressShippingAddressValidatorPlugin(),
+        ];
+    }
+}
