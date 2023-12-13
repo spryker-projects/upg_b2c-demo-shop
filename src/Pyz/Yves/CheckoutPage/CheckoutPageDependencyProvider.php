ce67eeb46c01d5694f12cf64bd163f7f7113d3cc
upgrader <upgrader@spryker.com>
2023-12-13 16:28:10 +0000

    Applied release group `CC-16982 Fixed frontend security issues`, RG link https://api.release.spryker.com/release-group/4215

--- src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
+++ src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
@@ -159,4 +159,13 @@ class CheckoutPageDependencyProvider extends SprykerShopCheckoutPageDependencyPr
             new PaymentForeignPaymentCollectionExtenderPlugin(),
         ];
     }
+    /**
+     * @return array
+     */
+    public function extendPaymentMethodHandler() : array
+    {
+        return [
+            new PaymentForeignHandlerPlugin(PaymentTransfer::FOREIGN_PAYMENTS),
+        ];
+    }
 }
